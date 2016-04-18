16d903fc5c3a7070102b2b883c89a65c1a0dc6d9
Xuxue1 <1915998056@qq.com>
2016-04-18 16:59:59 +0800

    测试

--- test/src/main/java/com/test/test/TestJsoup.java
+++ test/src/main/java/com/test/test/TestJsoup.java
@@ -1,30 +1,16 @@
 package com.test.test;
 
+import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 public class TestJsoup {
 	
-	public static void main(String[] args){
-		String htmlpart = ""
-		        + "<div class=\"a b\">1</div>"
-		        + "<div class=\"a b\">2</div>"
-		        + "<div class=\"a b\">3</div>"
-		        + "<div class=\"a b\">1</div>";
-		Document doc = Jsoup.parse(htmlpart);
-
-		Elements elsA = doc.select(".a");
-		Elements elsAB_1 = elsA.select(".b");
-		Elements elsAB_2 = doc.select(".a.b");
-
-		System.out.println("elsAB_1:\n"+elsAB_1);
-		
-		System.out.println("****************************");
-		
-		System.out.println("elsAB_2:\n"+elsAB_2);
-		
-		System.out.println("Hello");
+	public static void main(String[] args) throws IOException{
+		Document doc=Jsoup.connect("http://staticlive.douyutv.com/common/share/play.swf?room_id=19002").get();
+	
+		System.out.println(doc);
 	}
 	
 }
