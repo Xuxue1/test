aab613a6255a870ea2990d1c44deed5ffce00493
Xuxue1 <1915998056@qq.com>
2016-04-18 17:28:10 +0800

    测试分支是不是能用

---
+++ test/src/main/java/com/xuxue/test/App.java
@@ -0,0 +1,14 @@
+package com.xuxue.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("测试分支是不是能使用");
+    }
+}
